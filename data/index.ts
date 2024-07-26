64041cfc406b61914f8438239030b5e06566748b
Deepanshu Tomar <tomardeepanshu12@gmail.com>
2024-07-27 00:05:06 +0530

    Completed

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Realtime Chat application",
+    des: "Increased user engagement by developing a dynamic robust application using React, Node.js, Express.js, Socket.io, and MongoDB, showcasing proficiency in cutting-edge web technologies and fostering seamless communication experiences.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://github.com/deepanshutomarr/react_realtime_chat_app.git",
+  },
+  {
+    id: 2,
+    title: "Brew Crew - Coffee Ordering App",
+    des: "Developed a mobile app using Flutter and Firebase featuring user authentication with Firebase Authentication, a dynamic menu display using Fire-store, and real-time order tracking with Firebase Realtime Database.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://github.com/deepanshutomarr/brew_crew.git",
+  },
+  {
+    id: 3,
+    title: "Catalogue Application",
+    des: "Deployed and organized the development of a product catalogue app, driving a 15% surge in user engagement. Delivered comprehensive marketing collateral, empower users to discern and execute well-informed purchase decisions seamlessly.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://github.com/deepanshutomarr/Flutter-Catalog-App.git",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Deepanshu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Deepanshu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Deepanshu is the ideal partner.",
+    // name: "Michael Johnson",
+    title: "Scientist at Defence Research and Development Organisation",
+  },
+  {
+    quote:
+      "Collaborating with Deepanshu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Deepanshu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Deepanshu is the ideal partner.",
+    // name: "Michael Johnson",
+    title: "Scientist at Defence Research and Development Organisation",
+  },
+  {
+    quote:
+      "Collaborating with Deepanshu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Deepanshu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Deepanshu is the ideal partner.",
+    // name: "Michael Johnson",
+    title: "Scientist at Defence Research and Development Organisation",
+  },
+  {
+    quote:
+      "Collaborating with Deepanshu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Deepanshu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Deepanshu is the ideal partner.",
+    // name: "Michael Johnson",
+    title: "Scientist at Defence Research and Development Organisation",
+  },
+  {
+    quote:
+      "Collaborating with Deepanshu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Deepanshu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Deepanshu is the ideal partner.",
+    // name: "Michael Johnson",
+    title: "Scientist at Defence Research and Development Organisation",
+  },
+];
+
+// export const companies = [
+//   {
+//     id: 1,
+//     name: "cloudinary",
+//     img: "/cloud.svg",
+//     nameImg: "/cloudName.svg",
+//   },
+//   {
+//     id: 2,
+//     name: "appwrite",
+//     img: "/app.svg",
+//     nameImg: "/appName.svg",
+//   },
+//   {
+//     id: 3,
+//     name: "HOSTINGER",
+//     img: "/host.svg",
+//     nameImg: "/hostName.svg",
+//   },
+//   {
+//     id: 4,
+//     name: "stream",
+//     img: "/s.svg",
+//     nameImg: "/streamName.svg",
+//   },
+// {
+//   id: 5,
+//   name: "docker.",
+//   img: "/dock.svg",
+//   nameImg: "/dockerName.svg",
+// },
+// ];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Research Trainee",
+    desc: "Achieved a 20% reduction in coding violations and improved security flaw detection by 15% using static code analysis techniques. Learned diverse programming tools and their application, leading to a 25% increase in accuracy of the identification of issues. Executed Find Bugs on the SARD synthetic code base, diminishing identified issues by 15% and improving code quality through calculated metrics (coverage, recall, precision).",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Software Engineer Intern",
+    desc: "Developed responsive web applications in HTML, CSS, JavaScript, and Node.js programming languages, showcasing technical aptitude, flexibility, and led to an increase of 10% efficiency. Identified and initiated cross-functional activities in team which resulted in 15% improvement on project quality with client satisfaction.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Software Engineer Intern",
+    desc: "Designed cross-platform mobile applications using Flutter, Firebase, and Dart programming language, which reduces development time by 20% and enhancing app performance by 15%. Integrated libraries and debugging tools which contributes to a 10% boost in app functionality and a 20% reduction in time to resolve software issues ensuring prompt resolution.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Senior Core Member",
+    desc: "Led the planning and coordination of 3 and more workshops and events for VIT Vellore’s Technical-Fest, attracting over 100+ attendees and increasing student engagement by 20% annually. Guided 6 to 7 app and web development projects and ensured active involvement from their side in the complete project lifecycle, and resulting in an improvement of 20%.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/deepanshutomarr"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/deepanshu5051"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "http://www.linkedin.com/in/deepanshutomar"
+  },
+];
